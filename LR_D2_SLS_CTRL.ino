6073b78bd943e0878eca2321e0e037738d11248b
LOX <139780480+l-ox@users.noreply.github.com>
2023-07-19 02:49:48 +0100

    Update LR_D2_SLS_CTRL.ino
    Simplified the structure by splitting out into functions for raiseVehicle, lowerVehicle, warn, and deactivate.
    
    Fixed bug where program would lock in a loop if you tried to select another mode whilst in the process of raising or lowering.
    
    Added function for warning signal (flash warning lamp and warning tone on vehicle binnacle during operation). Used Millis to avoid having to use program delay.
    
    Added Off-Road Mode warning lamp (solid warning lamp on vehicle binnacle when current_mode == 3).
    
    Updated comments and tidied formatting.

--- LR_D2_SLS_CTRL.ino
+++ LR_D2_SLS_CTRL.ino
@@ -6,18 +6,19 @@ const int os_solenoid = 3; // Assign pin for O/S/R solenoid.
 const int ns_solenoid = 4; // Assign pin for N/S/R solenoid.
 const int vent_solenoid = 5; // Assign pin for vent solenoid.
 const int pump_relay = 6; // Assign pin for pump relay.
-const int warn_lamp = 7; // Assign pin for warning lamp.
-const int status_lamp = 8; // Assign pin for status lamp.
-const int warn_tone = 9; // Assign pin for warning tone.
+const int warn_sig = 7; // Assign pin for warning (lamp and tone).
+const int orm_status_lamp = 8; // Assign pin for Off-Road Mode status lamp.
 const int height_deviation = 50; // Variable to set the max allowed +/- deviation before system activation.
-int target_mode = 3; // Variable to store target mode.
-int last_msb_state = LOW; // Variable to store the previous MSB state.
+int target_mode = 3; // Variable to store target mode, and set default to 3, which corrects in the program as 0 at first wakeup.
+int last_msb_state = LOW; // Variable to store the previous MSB state, and set default to LOW.
 int msb_state; // Variable to store the current MSB state.
 int current_height; // Variable to store the current vehicle height.
 int current_mode; // Variable to store the current active mode.
 int target_height; // Variable to store target vehicle height.
-int direction; // Variable to store the current attempted lift direction.
-int activation_status = 1; // Variable to store the current activation status.
+int activation_status = 1; // Variable to store the current activation status - on by default to level vehicle at first wakeup.
+int warn_sig_state = LOW; // Variable to store warning signal state, and set default to LOW.
+unsigned long previousMillis = 0; // Variable to store previousMillis time, and set default to 0.
+const long interval = 500; // Variable to store interval for warning signal, default to 500.
 
 void setup() {
   pinMode(msb_pin, INPUT_PULLUP); // Set the MSB pin as an input with an internal pull-up resistor.
@@ -25,18 +26,15 @@ void setup() {
   pinMode (ns_solenoid, OUTPUT); // Set the N/S/R pin as an output.
   pinMode (vent_solenoid, OUTPUT); // Set the vent solenoid pin as an output.
   pinMode (pump_relay, OUTPUT); // Set the pump relay pin as an output.
-  pinMode (warn_lamp, OUTPUT); // Set the warning lamp pin as an output.
-  pinMode (status_lamp, OUTPUT); // Set the status lamp pin as an output.
-  pinMode (warn_tone, OUTPUT); // Set the warning tone pin as an output.
+  pinMode (orm_status_lamp, OUTPUT); // Set the status lamp pin as an output.
+  pinMode (warn_sig, OUTPUT); // Set the warning signal pin as an output.
   Serial.begin(9600); // Initialise the Serial Monitor.
 }
 
 void loop() {
-  msb_state = digitalRead(msb_pin); // Read the current state from the MSB*
-  current_height = analogRead(A0);
-  // Check if the MSB state has changed from LOW to HIGH (MSB depressed)*
-  if (msb_state == HIGH && last_msb_state == LOW) {
-    // Change the mode in sequence upon MSB activation and update Serial Monitor*
+  msb_state = digitalRead(msb_pin); // Read the current state from the MSB pin and store it in the msb_state variable.
+  current_height = analogRead(A0); // Read the current vehicle height from the sensor and store in the current_height variable.
+  if (msb_state == HIGH && last_msb_state == LOW) { // Check if the MSB state has changed from LOW to HIGH (MSB depressed), change the target mode in sequence upon MSB activation, and update Serial Monitor.
     target_mode = (target_mode + 1) % 4;
     activation_status = 1;
   }
@@ -55,50 +53,82 @@ void loop() {
   if (target_mode == 3) {
     target_height = 800;
   }
-  // Attempt to raise vehicle to target height.
-  if (current_height < (target_height - height_deviation) && direction == 0 && activation_status == 1) {
+  // Detect if system is activated and the current height is less than the target height (minus the deviation). If true, call for the vehicle to be raised. Update Serial Monitor.
+  if (current_height < (target_height - height_deviation) && activation_status == 1) {
+    Serial.print("Attempting to raise vehicle to target mode = ");
+    Serial.println(target_mode);
+    Serial.print("Current height = ");
+    Serial.println(current_height);
+    Serial.print("Target height = ");
+    Serial.println(target_height);
+    raiseVehicle();
+  }
+  // Detect if system is activated and the current height is more than the target height (minus the deviation). If true, call for the vehicle to be lowered. Update Serial Monitor.
+  if (current_height > (target_height + height_deviation) && activation_status == 1) {
+    Serial.print("Attempting to lower vehicle to target mode = ");
+    Serial.println(target_mode);
+    Serial.print("Current height = ");
+    Serial.println(current_height);
+    Serial.print("Target height = ");
+    Serial.println(target_height);
+    lowerVehicle();
+  }
+  // Enable ORM lamp in mode 3, else disable.
+  if (current_mode == 3) {
+    digitalWrite(orm_status_lamp, HIGH);
+  }
+  if (current_mode != 3) {
+    digitalWrite(orm_status_lamp, LOW);
+  }
+}
+// Attempts to raise the vehicle to the target height, looping meanwhile to activate warning signal, and listens for further MSB signal to breakout. If target height is reached, run deactivation. If MSB is depressed, break out of the loop. Update Serial Monitor.
+void raiseVehicle() {
+    while (current_height < (target_height - height_deviation) && activation_status == 1 && msb_state == HIGH) {
     digitalWrite(os_solenoid, HIGH);
     digitalWrite(ns_solenoid, HIGH);
     digitalWrite(pump_relay, HIGH);
-    direction = 2;
-    Serial.print("Attempting to raise vehicle to target height = ");
-    Serial.println(target_mode);
-    Serial.print("Current height = ");
-    Serial.println(current_height);
-  }
-  // Deactivate system, vehicle reached target height successfully (UP).
-   if (current_height > target_height && direction == 2 && activation_status ==1) {
-    digitalWrite(os_solenoid, LOW);
-    digitalWrite(ns_solenoid, LOW);
-    digitalWrite(pump_relay, LOW);
-    direction = 0;
-    current_mode = target_mode;
-    activation_status = 0;
-    Serial.println("Raised to target height successfully.");
-    Serial.print("Current height = ");
-    Serial.println(current_height);
-   }
-  // Attempt to lower vehicle to target height.
-   if (current_height > (target_height + height_deviation) && direction == 0 && activation_status == 1) {
+    current_height = analogRead(A0);
+    msb_state = digitalRead(msb_pin);
+    warn();
+    }
+  deactivate();
+}
+// Attempts to lower the vehicle to the target height, looping meanwhile to activate warning signal, and listens for further MSB signal to breakout. If target height is reached, run deactivation. If MSB is depressed, break out of the loop. Update Serial Monitor.
+void lowerVehicle() {
+    while (current_height > (target_height + height_deviation) && activation_status == 1 && msb_state == HIGH) {
     digitalWrite(os_solenoid, HIGH);
     digitalWrite(ns_solenoid, HIGH);
     digitalWrite(vent_solenoid, HIGH);
-    direction = 1;
-    Serial.print("Attempting to lower vehicle to target height = ");
-    Serial.println(target_mode);
-    Serial.print("Current height = ");
-    Serial.println(current_height);
-   }
-  // Deactivate system, vehicle reached target height successfully (DOWN).
-   if (current_height < target_height && direction == 1 && activation_status == 1) {
+    current_height = analogRead(A0);
+    msb_state = digitalRead(msb_pin);
+    warn();
+    }
+  deactivate();
+}
+// Activate warning signal (warning light flash and warning tone on vehicle binnacle) - flash/sound every 500ms.
+void warn() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if (warn_sig_state == LOW) {
+      warn_sig_state = HIGH;
+    }
+    else {
+      warn_sig_state = LOW;
+    }
+    digitalWrite(warn_sig, warn_sig_state);
+  }
+}
+// Deactivate all SLS elements due to successful operation. Update current_mode variable. Disable warning signal. Update Serial Monitor.
+void deactivate() {
     digitalWrite(os_solenoid, LOW);
     digitalWrite(ns_solenoid, LOW);
     digitalWrite(vent_solenoid, LOW);
-    direction = 0;
-    current_mode = target_mode;
-    activation_status = 0;
-    Serial.println("Lowered to target height successfully.");
+    digitalWrite(pump_relay, LOW);
+    Serial.println("Vehicle raised to target height successfully.");
     Serial.print("Current height = ");
     Serial.println(current_height);
-   }
+    current_mode = target_mode;
+    activation_status = 0;
+    digitalWrite(warn_sig, LOW);
 }
